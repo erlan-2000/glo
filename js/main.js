becf1020a862427bd0082994df4a4deae914fbaa
Erlan Sulaimanov <ersulaimanov@gmail.com>
2019-10-21 00:20:06 +0300

    added mobile navigation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+const menuToggle = document.querySelector('#menu-toggle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+
+menuToggle.onclick = function() {
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active');
+}
+
+
